8011ae3cee3591009823e36e013952eff35a3287
Nathan Heaivilin <22554140+heaivilin@users.noreply.github.com>
2022-11-17 14:15:13 -0600

    demo changes

--- Northwinds/Northwinds/SSDT/Table/PatrickMcHolmes.sql
+++
@@ -1,7 +0,0 @@
-﻿CREATE TABLE [SSDT].[PatrickMcHolmes]
-(
-	[Id] INT NOT NULL PRIMARY KEY
-	, passRating FLOAT
-	, UniformNumber TINYINT
-	, YardsPerCarry INT
-)
---
+++ Northwinds/Northwinds/dbo/Tables/BobIsAwesome.sql
@@ -0,0 +1,7 @@
+﻿CREATE TABLE [dbo].[BobIsAwesome]
+(
+	[Id] INT NOT NULL PRIMARY KEY
+	, col1 int
+	, col2 char(10), 
+    [col4 ] BIT NOT NULL DEFAULT 1
+)
